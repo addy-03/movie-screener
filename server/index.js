3fc478151bf1cc0a3bbc3296b0e80500cd151330
Aditya Wagh <adityawagh169@gmail.com>
2023-04-08 02:30:48 +0530

    Modified CORS policy

--- server/index.js
+++ server/index.js
@@ -7,20 +7,33 @@ const PORT = process.env.PORT || 5000;
 const reqHeadSuccess = { "Content-Type": "application/json" };
 
 const server = http.createServer(async (req, res) => {
+  // Set CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Request-Method", "*");
+  res.setHeader("Access-Control-Allow-Methods", ["GET", "POST", "OPTIONS"]);
+  res.setHeader("Access-Control-Allow-Headers", "*");
+
   // Set the request route
   if (req.url === "/api" && req.method === "GET") {
+    console.log("/api");
     res.writeHead(200, reqHeadSuccess);
     res.write("This is Movie Screener API");
     res.end();
+  } else if (req.method === "OPTIONS") {
+    res.writeHead(200);
+    res.end();
+    return;
   }
   // GET all the movies
   else if (req.url === "/movies" && req.method === "GET") {
+    console.log("/movies");
     const movies = await new Movies().getAllMovies();
     res.writeHead(200, reqHeadSuccess);
     res.end(JSON.stringify(movies));
   }
   // GET Labels
   else if (req.url === "/movies/labels" && req.method === "GET") {
+    console.log("/movies/labels");
     const labels = await new Movies().getMovieLabels();
     res.writeHead(200, reqHeadSuccess);
     res.end(JSON.stringify(labels));
@@ -30,6 +43,7 @@ const server = http.createServer(async (req, res) => {
     req.url.match(/^\/movies\/shortlist\/[\w(%20)+]+$/) &&
     req.method === "PATCH"
   ) {
+    console.log(req.url);
     fs.readFile(moviesDataFile, "utf-8", (error, data) => {
       if (error) {
         res.writeHead(404, { "Content-Type": "application/json" });
@@ -46,6 +60,7 @@ const server = http.createServer(async (req, res) => {
     req.url.match(/\/movies\?filter=[\w(%20)+]+$/) &&
     req.method === "GET"
   ) {
+    console.log(req.url);
     const label = req.url.split("=")[1].replace("%20", " ");
     console.log(label);
     const filteredData = await new Movies().getMoviesByLabel(label);
@@ -54,6 +69,7 @@ const server = http.createServer(async (req, res) => {
   }
   // If no route present
   else {
+    console.log(req.url);
     res.writeHead(404, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ message: "Route not found" }));
   }
